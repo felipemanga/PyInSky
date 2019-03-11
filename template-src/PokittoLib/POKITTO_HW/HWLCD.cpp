263d255c31ef8b258b5678469e570847b5e45b20
FManga <fmanga@localhost.localdomain>
2019-03-11 23:36:33 +0000

    fixed pixel 0 bug

--- template-src/PokittoLib/POKITTO_HW/HWLCD.cpp
+++ template-src/PokittoLib/POKITTO_HW/HWLCD.cpp
@@ -1071,14 +1071,14 @@ void Pokitto::lcdRefreshMode1Spr(
 
 
 #define MODE2_INNER_LOOP_B				\
-  "	ldm %[scanline]!, {%[c]}"   "\n"		\
 	       "	str %[c], [%[LCD], 0]"    "\n"	\
-	       "	str %[t], [%[LCD], 124]"  "\n"	\
-	       "	movs %[c], 252"   "\n"		\
+	       "	movs %[c], 252"		  "\n"	\
 	       "	str %[t], [%[LCD], %[c]]" "\n"	\
-	       "	str %[t], [%[LCD], 124]"  "\n"	\
 	       "	subs %[x], 1"             "\n"	\
-	       "	str %[t], [%[LCD], %[c]]" "\n"	\
+	       "	str %[t], [%[LCD], 124]"  "\n"	\
+	       "	str %[t], [%[LCD], %[c]]" "\n"  \
+    	       "	ldm %[scanline]!, {%[c]}" "\n"	\
+	       "	str %[t], [%[LCD], 124]"  "\n"
 
 
 void Pokitto::lcdRefreshMode2(uint8_t * scrbuf, uint16_t* paletteptr ) {
@@ -1091,95 +1091,95 @@ void Pokitto::lcdRefreshMode2(uint8_t * scrbuf, uint16_t* paletteptr ) {
   write_command(0x21);  // Vertical DRAM Address
 
 #ifndef __ARMCC_VERSION
-  write_data(1); // still has pixel 0 bug
+  write_data(0); // pixel 0 bug fixed
   write_command(0x22); // write data to DRAM
   CLR_CS_SET_CD_RD_WR;
   SET_MASK_P2;
 
-  asm volatile(
-	 ".syntax unified"         "\n"
+    asm volatile(
+	".syntax unified"         "\n"
 
-	 "mov r10, %[scanline]"    "\n"
-	 "mov r11, %[t]"           "\n"
+	"mov r10, %[scanline]"    "\n"
+	"mov r11, %[t]"           "\n"
 
-	 "mode2OuterLoop:"        "\n"
+	"mode2OuterLoop:"        "\n"
 
-	 "movs %[x], 110"          "\n"
-	 "mode2InnerLoopA:"
+	"movs %[x], 110"          "\n"
+	"mode2InnerLoopA:"
 
 
-	 "	ldrb %[byte], [%[scrbuf],0]"   "\n"
-	 "	lsrs %[c], %[byte], 4"    "\n"
+	"	ldrb %[byte], [%[scrbuf],0]"   "\n"
+	"	lsrs %[c], %[byte], 4"    "\n"
 
-	 "	movs %[t], 15" "\n"
-	 "	ands %[byte], %[t]"    "\n"
+	"	movs %[t], 15" "\n"
+	"	ands %[byte], %[t]"    "\n"
 
-	 "	lsls %[c], 1"             "\n"
-	 "	ldrh %[t], [%[paletteptr], %[c]]"      "\n"
-	 "	lsls %[t], %[t], 3"       "\n"
-	 "	str %[t], [%[LCD], 0]"    "\n"
-	 "	mov %[c], r11" "\n"
-	 "	str %[c], [%[LCD], 124]"  "\n"
-	 "	stm %[scanline]!, {%[t]}" "\n"
-	 "	movs %[t], 252"   "\n"
-	 "	str %[c], [%[LCD], %[t]]" "\n"
-	 "	str %[c], [%[LCD], 124]"  "\n"
-	 "	lsls %[byte], %[byte], 1"             "\n"
-	 "	str %[c], [%[LCD], %[t]]" "\n"
+	"	lsls %[c], 1"             "\n"
+	"	ldrh %[t], [%[paletteptr], %[c]]"      "\n"
+	"	lsls %[t], %[t], 3"       "\n"
+	"	str %[t], [%[LCD], 0]"    "\n"
+	"	stm %[scanline]!, {%[t]}" "\n"
+	"	mov %[c], r11" "\n"
+	"	movs %[t], 252"   "\n"
+	"	str %[c], [%[LCD], %[t]]" "\n"
+	"	lsls %[byte], %[byte], 1"             "\n"
+	"	str %[c], [%[LCD], 124]"  "\n"
+	"	str %[c], [%[LCD], %[t]]" "\n"
+	"	ldrh %[t], [%[paletteptr], %[byte]]"      "\n"	
+	"	str %[c], [%[LCD], 124]"  "\n"
 
-	 "	ldrh %[t], [%[paletteptr], %[byte]]"      "\n"
-	 "	lsls %[t], %[t], 3"       "\n"
-	 "	str %[t], [%[LCD], 0]"    "\n"
-	 "	mov %[c], r11" "\n"
-	 "	str %[c], [%[LCD], 124]"  "\n"
-	 "	stm %[scanline]!, {%[t]}" "\n"
-	 "	movs %[t], 252"   "\n"
-	 "	str %[c], [%[LCD], %[t]]" "\n"
-	 "	str %[c], [%[LCD], 124]"  "\n"
-	 "	adds %[scrbuf], %[scrbuf], 1" "\n"
-	 "	str %[c], [%[LCD], %[t]]" "\n"
+	"	lsls %[t], %[t], 3"       "\n"
+	"	str %[t], [%[LCD], 0]"    "\n"
+	"	stm %[scanline]!, {%[t]}" "\n"
+	"	mov %[c], r11" "\n"
+	"	movs %[t], 252"   "\n"
+	"	str %[c], [%[LCD], %[t]]" "\n"
+	"	adds %[scrbuf], %[scrbuf], 1" "\n"
+	"	str %[c], [%[LCD], 124]"  "\n"
+	"	str %[c], [%[LCD], %[t]]" "\n"
+	"	subs %[x], 2"          "\n"
+	"	str %[c], [%[LCD], 124]"  "\n"
+	"	bne mode2InnerLoopA"  "\n"
 
-	 "	subs %[x], 2"          "\n"
-	 "	bne mode2InnerLoopA"  "\n"
+	"mov %[scanline], r10"    "\n"
+	"movs %[x], 110"          "\n"
+	"mov %[t], r11"           "\n"
+    	       "	ldm %[scanline]!, {%[c]}" "\n"
+	"mode2InnerLoopB:"
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	MODE2_INNER_LOOP_B
+	"	bne mode2InnerLoopB"     "\n"
 
-	 "mov %[scanline], r10"    "\n"
-	 "movs %[x], 110"          "\n"
-	 "mov %[t], r11"           "\n"
-	 "mode2InnerLoopB:"
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 MODE2_INNER_LOOP_B
-	 "	bne mode2InnerLoopB"     "\n"
+	"mov %[scanline], r10"    "\n"
+	"movs %[t], 1"              "\n"
+	"movs %[c], 88"             "\n"
+	"add %[y], %[t]"            "\n" // y++... derpy, but it's the outer loop
+	"cmp %[y], %[c]"            "\n"
+	"bne mode2OuterLoop"       "\n" // if y != 88, loop
 
-	 "mov %[scanline], r10"    "\n"
-	 "movs %[t], 1"              "\n"
-	 "movs %[c], 88"             "\n"
-	 "add %[y], %[t]"            "\n" // y++... derpy, but it's the outer loop
-	 "cmp %[y], %[c]"            "\n"
-	 "bne mode2OuterLoop"       "\n" // if y != 88, loop
+	: // outputs
+	  [c]"+l" (c),
+	  [t]"+l" (t),
+	  [x]"+l" (x),
+	  [y]"+h" (y),  // +:Read-Write l:lower (0-7) register
+	  [scrbuf]"+l" (scrbuf)
 
-	 : // outputs
-	   [c]"+l" (c),
-	   [t]"+l" (t),
-	   [x]"+l" (x),
-	   [y]"+h" (y),  // +:Read-Write l:lower (0-7) register
-	   [scrbuf]"+l" (scrbuf)
-
-	 : // inputs
-	   [LCD]"l" (0xA0002188),
-	   [scanline]"l" (scanline),
-	   [paletteptr]"l" (paletteptr),
-	   [byte]"l" (byte)
-	 : // clobbers
-	   "cc", "r10", "r11"
-       );
+	: // inputs
+	  [LCD]"l" (0xA0002188),
+	  [scanline]"l" (scanline),
+	  [paletteptr]"l" (paletteptr),
+	  [byte]"l" (byte)
+	: // clobbers
+	  "cc", "r10", "r11"
+	);
 
 
 #else
